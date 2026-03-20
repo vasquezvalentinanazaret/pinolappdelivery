ed8e71fb1b5a083d7a1f26861ae571b06d14dea0
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-20 09:58:47 -0600

    Create AppNavigator.tsx

---
+++ apps/user-mobile-app/src/navigation/AppNavigator.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import SplashScreen from "../screens/SplashScreen/SplashScreen";
+import LoginScreen from "../screens/LoginScreen/LoginScreen";
+import HomeScreen from "../screens/HomeScreen/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
