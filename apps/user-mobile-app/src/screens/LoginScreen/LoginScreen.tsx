4854f3caf705c9b57ec39b967b4f9a277aac4d51
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-20 09:56:38 -0600

    Create LoginScreen.tsx

---
+++ apps/user-mobile-app/src/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // 🔐 luego conectamos backend
+    console.log("Login:", email, password);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bienvenido a PinolApp 🇳🇮</Text>
+
+      <TextInput
+        placeholder="Correo"
+        placeholderTextColor="#aaa"
+        style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      <TextInput
+        placeholder="Contraseña"
+        placeholderTextColor="#aaa"
+        secureTextEntry
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Iniciar Sesión</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0f172a",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    color: "#22c55e",
+    fontSize: 22,
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  input: {
+    backgroundColor: "#1e293b",
+    color: "#fff",
+    padding: 12,
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#22c55e",
+    padding: 15,
+    borderRadius: 8,
+    marginTop: 10,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#000",
+    fontWeight: "bold",
+  },
+});
