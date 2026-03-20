09f986427fccfacdb55923524889bd4cbabb35e0
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-20 10:17:42 -0600

    Update App.tsx

--- apps/user-mobile-app/App.tsx
+++ apps/user-mobile-app/App.tsx
@@ -1,6 +1,14 @@
 import React from "react";
-import AppNavigator from "./src/navigation/AppNavigator";
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from "./src/navigation";
+import { CartProvider } from "./src/context/CartContext";
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <CartProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </CartProvider>
+  );
 }
