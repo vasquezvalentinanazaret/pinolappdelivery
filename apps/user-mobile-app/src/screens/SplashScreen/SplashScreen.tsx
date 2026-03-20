a498941515dc1512790d0e8bb1e894db0d4ba2e3
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-20 10:03:25 -0600

    Update SplashScreen.tsx

--- apps/user-mobile-app/src/screens/SplashScreen/SplashScreen.tsx
+++ apps/user-mobile-app/src/screens/SplashScreen/SplashScreen.tsx
@@ -1,1 +1,41 @@
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
 navigation.replace("Login");
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // luego irá al login
+      console.log("Ir a Login...");
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.logo}>PinolApp 🇳🇮</Text>
+      <Text style={styles.subtitle}>Delivery Nicaragüense</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0f172a",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    color: "#22c55e",
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    color: "#fff",
+    marginTop: 10,
+    fontSize: 14,
+  },
+});
