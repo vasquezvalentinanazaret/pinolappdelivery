572ddd2e403823e84edd0a711db85511f18654f8
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-20 09:59:28 -0600

    Update App.tsx

--- apps/user-mobile-app/App.tsx
+++ apps/user-mobile-app/App.tsx
@@ -1,22 +1,6 @@
 import React from "react";
-import { View, Text } from "react-native";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#0f172a",
-      }}
-    >
-      <Text style={{ color: "#22c55e", fontSize: 24 }}>
-        PinolApp 🇳🇮
-      </Text>
-      <Text style={{ color: "#fff", marginTop: 10 }}>
-        Delivery Nicaragüense
-      </Text>
-    </View>
-  );
+  return <AppNavigator />;
 }
